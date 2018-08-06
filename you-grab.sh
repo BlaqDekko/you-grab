159c2b20559ec9cdcab536bb44d66ba2722e004e
Blake Kliewer <blaqklie@gmail.com>
2018-08-06 13:43:13 -0700

    Adding first pass at the bash script.

---
+++ you-grab.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+read -p "Enter Your YouTube URL: "  ytlink
+cd
+cd ~/Dropbox/YouTube_Rips/
+echo "Attempting to download highest qualiity for $ytlink."
+youtube-dl -f 137+140 $ytlink || true
+read -p "Did it work? (y/n) " answer
+if [[ $answer == "y" || $answer == "yes" ]]; then
+  dropbox start
+  exit
+elif [[ $answer == "n" || $answer == "no" ]]
+then
+  echo "Trying again..."
+else
+  echo "You didn't answer with yes or no. Exiting..."
+  exit
+fi
+youtube-dl -f 136+140 $ytlink
+read -p "Did it work this time? (y/n) " answer2
+if [[ $answer2 == "y" || $answer2 == "yes" ]]; then
+  dropbox start
+  exit
+elif [[ $answer2 == "n" || $answer2 == "no" ]]
+then
+  echo "Sorry. No DASH version available in 1080p or 720p. Try 'youtube-dl -F'."
+else echo "You didn't answer with yes or no, dummy. Exiting..."
+  exit
+fi
+echo "Exiting..."
+exit
